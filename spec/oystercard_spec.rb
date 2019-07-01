711b1e4feebd5cc08ddb30061340fbb6122081a0
Josh Hemsley <josh@breathehr.com>
2019-07-01 14:28:35 +0100

    completed step 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  subject = Oystercard.new
+  it 'card has default balance of zero when initialized' do
+    expect(subject.balance).to eq(0)
+  end
 end
