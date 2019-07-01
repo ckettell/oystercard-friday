3b516703e5d7b2055964508a9da14bcb654b7e4b
Student <student@Makerss-MacBook-Pro.local>
2019-07-01 14:47:17 +0100

    add maximum value check for top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,17 @@
 class Oystercard
 attr_reader :balance
 
+MAXIMUM_VALUE = 90.00
+
   def initialize
     @balance = 0
   end
   def top_up(value)
     @balance += value
+    raise 'Exceeded maximum top-up value' if maximum_balance_exceeded?
+  end
+  def maximum_balance_exceeded?
+    @balance > MAXIMUM_VALUE
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,12 @@ describe Oystercard do
     subject.top_up(5.00)
     expect(subject.balance).to eq (5.00)
   end
+  it 'raises an error if card topped up over maximum value' do
+    expect { subject.top_up(Oystercard::MAXIMUM_VALUE+1) }.to raise_error 'Exceeded maximum top-up value'
+  end
+  it 'checks if balance is over £90' do
+    subject.top_up(Oystercard::MAXIMUM_VALUE+1)
+    expect(subject.maximum_balance_exceeded?).to eq true
+  end
+
 end
