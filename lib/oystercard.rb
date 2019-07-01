80a08129882656b2373a2c22527cffb255a2109e
Student <student@Makerss-MacBook-Pro.local>
2019-07-01 14:36:17 +0100

    add the top_up method step5

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,6 +4,8 @@ attr_reader :balance
   def initialize
     @balance = 0
   end
-
+  def top_up(value)
+    @balance += value
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-  subject = Oystercard.new
   it 'card has default balance of zero when initialized' do
     expect(subject.balance).to eq(0)
+  end
+  it 'tops up card balance' do
+    subject.top_up(5.00)
+    expect(subject.balance).to eq (5.00)
   end
 end
