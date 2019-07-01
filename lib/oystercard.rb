51ee46d3df73b2766e10abaa2fe387d76af734d1
Josh Hemsley <josh@breathehr.com>
2019-07-01 15:07:36 +0100

    completed step 7

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,10 +7,14 @@ MAXIMUM_VALUE = 90
   def initialize
     @balance = 0
   end
+
   def top_up(value)
     fail "Card balance cannot exceed £#{Oystercard::MAXIMUM_VALUE}" if @balance + value > MAXIMUM_VALUE
     @balance += value
   end
 
+  def deduct(fare)
+    @balance -= fare
+  end
   #binding.pry
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,14 +4,21 @@ describe Oystercard do
   it 'card has default balance of zero when initialized' do
     expect(subject.balance).to eq(0)
   end
+
   it 'tops up card balance' do
     subject.top_up(5)
     expect(subject.balance).to eq (5)
   end
+
   it 'raises an error if card topped up over maximum value' do
     subject.top_up(Oystercard::MAXIMUM_VALUE)
     expect { subject.top_up(1) }.to raise_error "Card balance cannot exceed £#{Oystercard::MAXIMUM_VALUE}"
   end
 
+  it 'deducts fare from card balance' do
+    subject.top_up(10)
+    subject.deduct(2)
+    expect(subject.balance).to eq (8)
+  end
 
 end
