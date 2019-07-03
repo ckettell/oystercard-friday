ee318fb5565c4d90aec405ef1af8fdb939f7cc79
Charles Kettell <charleskettell@hotmail.co.uk>
2019-07-03 17:32:03 +0100

    start and finish added to the journey log

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,22 +1,41 @@
 class JourneyLog
   attr_reader :journey_class
   attr_reader :current_journey
-  
+
   def initialize(journey_class = Journey.new)
     @journey_class = journey_class
     @in_journey = false
+    @journey_log = []
     @current_journey = []
   end
 
   def start(entry_station)
-    in_journey?
+    @in_journey = true
     @current_journey << {entry_station: entry_station}
+
+  end
+
+  def finish(exit_station)
+    @in_journey = false
+    @current_journey << {exit_station: exit_station}
+    @journey_log << @current_journey
+    @current_journey = []
+
   end
 
   def in_journey?
-    @in_journey = true
+    @in_journey
   end
 
+  def journeys
+    @journey_log.each do |journey|
+      return "Entry Station: #{@journey_log[:entry_station]}, Exit Station: #{@journey_log[:exit_station]}"
+    end
 
+  end
+
+  def current_journey
+    @current_journey
+  end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -17,13 +17,15 @@ describe JourneyLog do
       # expect(subject.current_journey[0]).to eq entry_station
       expect(subject.current_journey[0]).to include(:entry_station => entry_station)
     end
-
-
   end
 
-  # describe '#current_journey' do
-  #   it ''
-  # end
+  describe '#current_journey' do
+    it 'returns an incomplete journey or creates a new journey' do
+      subject.start(entry_station)
+      expect(subject.current_journey.length).to eq 1
+    end
+
+  end
 
   describe '#finish' do
     it 'ends a journey with an exit station' do
@@ -31,6 +33,22 @@ describe JourneyLog do
       subject.finish(exit_station)
       expect(subject.in_journey?).to eq false
     end
+    it 'adds an exit station to current journey' do
+      subject.start(entry_station)
+      subject.finish(exit_station)
+      expect(subject.current_journey[1]).to include(:exit_station => exit_station)
+    end
   end
+
+  describe '#journeys' do
+    it 'prints a list of previous journeys' do
+      subject.start(entry_station)
+      subject.finish(exit_station)
+      subject.start(entry_station)
+      subject.finish(exit_station)
+      expect(subject.journeys).to eq 'Entry Station: entry_station, Exit Station: exit_station \n Entry Station: entry_station, Exit Station: exit_station'
+    end
+  end 
+
 
 end
